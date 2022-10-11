4d7e353d40ce63cd29adf069402a9a33c85b4c81
Viswesh934 <98519767+Viswesh934@users.noreply.github.com>
2022-10-11 23:36:27 +0530

    #include<stdio.h> int main() {     int x,y,r,c,co,n;     scanf("%d%d",&x,&y);     for(;x<=y;x++)     {         n=x;         co=0;         c=0;         while(n)         {             r=n%10;             n/=10;             co++;             if(r==0)             break;             if(x%r==0)             c++;         }         if(c==co)         {             printf("%d ",x);         }     } }

---
+++ Self_Dividing_Numbers.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,r,c,co,n;
+    scanf("%d%d",&x,&y);
+    for(;x<=y;x++)
+    {
+        n=x;
+        co=0;
+        c=0;
+        while(n)
+        {
+            r=n%10;
+            n/=10;
+            co++;
+            if(r==0)
+            break;
+            if(x%r==0)
+            c++;
+        }
+        if(c==co)
+        {
+            printf("%d ",x);
+        }
+    }
+}
