7b23424f84dcef0e8dfca823583ad89884410b47
Viswesh934 <98519767+Viswesh934@users.noreply.github.com>
2022-10-11 23:52:51 +0530

    #include<stdio.h> int fun(int num) {     int i,fc=0;     for(i=1;i<=num;i++) 	{ 		if(num%i==0) 		{ 			fc++; 		} 	} 	if(fc==2) 	{ 		return 1; 	} 	else 	{ 		return 0; 	} } int main() {     int n,d,c=0,cc=0,temp;     scanf("%d",&n);     temp=n;     if(fun(n))     {         while(n)         {             d=n%10;             if(fun(d))             {                 cc++;             }             n=n/10;             c++;         }         if(c==cc)         {             printf("Mega Prime");         }         else         {             printf("Not Mega Prime");         }     }     else     {         printf("Not Mega Prime");     } }

---
+++ mega_prime.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+int fun(int num)
+{
+    int i,fc=0;
+    for(i=1;i<=num;i++)
+	{
+		if(num%i==0)
+		{
+			fc++;	
+		}	
+	}
+	if(fc==2)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+int main()
+{
+    int n,d,c=0,cc=0,temp;
+    scanf("%d",&n);
+    temp=n;
+    if(fun(n))
+    {
+        while(n)
+        {
+            d=n%10;
+            if(fun(d))
+            {
+                cc++;
+            }
+            n=n/10;
+            c++;
+        }
+        if(c==cc)
+        {
+            printf("Mega Prime");
+        }
+        else
+        {
+            printf("Not Mega Prime");
+        }
+    }
+    else
+    {
+        printf("Not Mega Prime");
+    }
+}
